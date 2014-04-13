8470cb11e7c794af4e687935a4b8f39d4cefac8a
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-04-13 13:30:08 +0800

    Finish GUI

--- ReadFromPixnet.java
+++ ReadFromPixnet.java
@@ -8,6 +8,7 @@ import java.net.URL;
 import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -23,6 +24,7 @@ public class ReadFromPixnet extends JApplet {
 	JButton run = new JButton("¨ú±o!");
 	JButton copy = new JButton("½Æ»s!");
 	JTextArea jta = new JTextArea();
+	JScrollPane jsp = new JScrollPane(jta);
 
 	public void init() {
 		setVisible(true);
@@ -35,39 +37,43 @@ public class ReadFromPixnet extends JApplet {
 		jtf.setBounds(0 * width, 1 * height / 10, 7 * width / 10, height / 10);
 		run.setBounds(7 * width / 10, 1 * height / 10, 3 * width / 10,
 				height / 10);
-		jta.setBounds(0 * width, 3 * height / 10, 1 * width, 7 * height / 10);
+		jsp.setBounds(0 * width, 3 * height / 10, 1 * width, 7 * height / 10);
 		run.addActionListener(get);
 		add(jl);
 		add(jl2);
 		add(jtf);
 		add(run);
-		add(jta);
+		add(jsp);
 	}
 
 	private ActionListener get = new ActionListener() {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			
+			try { // get json fromurl
+				String url = jtf.getText();
+				String[] token = url.split("/");
+				String[] arc = token[5].split("-");
+				String[] user = token[2].split("pixnet");
+				url = "http://emma.pixnet.cc/blog/articles/" + arc[0]
+						+ "?user=" + user[0].substring(0, user[0].length() - 1)
+						+ "&format=json";
+				URL pageurl = new URL(url);
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						pageurl.openStream(), "UTF-8"));
+				String json = "";
+				json = br.readLine(); // parse json
+				JSONObject obj = new JSONObject(json);
+				obj = new JSONObject(obj.get("article").toString());
+				HtmlCleaner cleaner = new HtmlCleaner();
+				TagNode node = cleaner.clean(obj.get("body").toString()
+						.replaceAll("&nbsp;", "").replaceAll("&ndash;", "-"));
+				jta.setText(node.getText().toString());
+			} catch (IOException e) {
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
 		}
 
 	};
-	/*
-	 * public static void main(String args[]) { BufferedReader in = new
-	 * BufferedReader(new InputStreamReader(System.in)); try { // get json from
-	 * url String url = in.readLine(); String[] token = url.split("/"); String[]
-	 * arc = token[5].split("-"); String[] user = token[2].split("pixnet"); url
-	 * = "http://emma.pixnet.cc/blog/articles/" + arc[0] + "?user=" +
-	 * user[0].substring(0, user[0].length() - 1) + "&format=json"; URL pageurl
-	 * = new URL(url); BufferedReader br = new BufferedReader(new
-	 * InputStreamReader( pageurl.openStream(), "UTF-8")); String json = "";
-	 * json = br.readLine(); // parse json JSONObject obj = new
-	 * JSONObject(json); obj = new JSONObject(obj.get("article").toString());
-	 * HtmlCleaner cleaner = new HtmlCleaner(); TagNode node =
-	 * cleaner.clean(obj.get("body").toString() .replaceAll("&nbsp;",
-	 * "").replaceAll("&ndash;", "-")); // Document doc =
-	 * Jsoup.parse(obj.get("body").toString());
-	 * System.out.println(node.getText()); } catch (IOException e) { } catch
-	 * (JSONException e) { e.printStackTrace(); } }
-	 */
 }
