8a5a60eba21683a055b1dcefecd29c08474f103c
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-05-16 16:31:32 +0800

    Oauth2.0 javadoc

--- PIXNET_Oauth1.0a/src/main/java/com/example/PostToPIXNETOauth2.java
+++ PIXNET_Oauth1.0a/src/main/java/com/example/PostToPIXNETOauth2.java
@@ -10,39 +10,63 @@ import java.net.URLEncoder;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-/** 
- * PostToPIXNETOauth2.0
+
+/**
+ * 
+ * @author Koi
+ * 
  */
-public class Main {
-	String url = "https://emma.pixnet.cc/oauth2/authorize?client_id=46857444204aa2ce0cb8c9666a8eadae&redirect_uri=http://oob&response_type=code";
+public class PostToPIXNETOauth2 {
 	String code = "";
 	String access_token = "";
+	String client_id = "";
+	String client_secret = "";
+	String redirect_uri = "";
+
 	/**
-	 * Main
+	 * Constructor
+	 * 
+	 * @param client_id
+	 *            your client id
+	 * @param client_secret
+	 *            your client secret
+	 * @param redirect_uri
+	 *            your redirect url
 	 * @throws IOException
 	 * @throws JSONException
 	 */
-	public Main() {
-		try{
-		grant();
-		post();
-		}catch(Exception e){
-		}
+	public PostToPIXNETOauth2(String client_id, String client_secret,
+			String redirect_uri) throws IOException, JSONException {
+		this.client_id = client_id;
+		this.client_secret = client_secret;
+		this.redirect_uri = redirect_uri;
 	}
-	/**
-	 * Start of Program
-	 * @param args
-	 * @throws IOException
-	 * @throws JSONException
-	 */
-	public static void main(String args[]) {
-		new Main();
+
+	public static void main(String args[]) throws IOException, JSONException {
+		PostToPIXNETOauth2 ptp = new PostToPIXNETOauth2(
+				"e6a0fa232cc4da68ae21b727b772229e",
+				"fac5c7f719feef5eea37449b1fc6b2ad", "http://oob");
+		System.out.println(ptp.post("759f5942086c063cbe53ce3d1c83c90e"));
 	}
+
 	/**
-	 * Function for Post
+	 * 
+	 * @return getGrant Url
+	 */
+	public String getGrant() {
+		String url = "https://emma.pixnet.cc/oauth2/authorize?client_id="
+				+ client_id + "&redirect_uri=" + redirect_uri
+				+ "&response_type=code";
+		return url;
+	}
+
+	/**
+	 * 
+	 * @param access_token
+	 * @return Post result
 	 * @throws IOException
 	 */
-	public void post() throws IOException {
+	public String post(String access_token) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in,
 				"big5"));
 		System.out.print("Title:");
@@ -54,7 +78,7 @@ public class Main {
 						+ access_token + "&body="
 						+ URLEncoder.encode(body, "UTF-8") + "&title="
 						+ URLEncoder.encode(title, "UTF-8")); //
-		System.out.println(urlPost.toString());
+		// System.out.println(urlPost.toString());
 		HttpURLConnection con = (HttpURLConnection) urlPost.openConnection();
 		con.setDoOutput(true);
 		con.setDoInput(true);
@@ -64,20 +88,23 @@ public class Main {
 		con.connect();
 		BufferedReader br = new BufferedReader(new InputStreamReader(
 				con.getInputStream(), "UTF-8"));
-		System.out.println(br.readLine());
+		return br.readLine();
 	}
+
 	/**
-	 * Function for get AccessToken
+	 * 
+	 * @param code
+	 * @return Set code and Get AccessToken
 	 * @throws IOException
 	 * @throws JSONException
 	 */
-	public void grant() throws IOException, JSONException {
-		System.out.println("Code:" + url);
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		code = in.readLine();
+	public String grant(String code) throws IOException, JSONException {
 		String urlgrant = "https://emma.pixnet.cc/oauth2/grant?grant_type=authorization_code&code="
 				+ code
-				+ "&redirect_uri=http://oob&client_id=46857444204aa2ce0cb8c9666a8eadae&client_secret=dfa46d6dc4acfc8a25046fffcc5d9b14";
+				+ "&redirect_uri="
+				+ redirect_uri
+				+ "&client_id="
+				+ client_id + "&client_secret=" + client_secret;
 		URL url = new URL(urlgrant);
 		URLConnection conn = url.openConnection();
 		BufferedReader br = new BufferedReader(new InputStreamReader(
@@ -85,8 +112,6 @@ public class Main {
 		String json = br.readLine();
 		JSONObject obj = new JSONObject(json);
 		access_token = (String) obj.get("access_token");
-	}
-	public int test(int a){
-		return 10;
+		return access_token;
 	}
 }
