ef84fac4193d54e7ed1b8249041b3f03d82611cb
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-05-16 13:35:58 +0800

    oauth1.0a done

---
+++ PIXNET_Oauth1.0a/src/test/java/com/example/MainTest.java
@@ -0,0 +1,38 @@
+package com.example;  
+  
+import static org.junit.Assert.assertEquals;  
+  
+import org.junit.Test;  
+import org.junit.runner.RunWith;  
+import org.junit.runners.JUnit4;  
+  
+/** 
+ * Tests for {@link Main}. 
+ */  
+@RunWith(JUnit4.class)  
+public class MainTest {  
+    @Test  
+    public void test() throws Exception{  
+	PostToPIXNET ptp = new PostToPIXNET("46857444204aa2ce0cb8c9666a8eadae",
+				"dfa46d6dc4acfc8a25046fffcc5d9b14");
+	System.out.println(ptp.request());
+    }  
+    @Test
+    public void set_nonce() throws Exception{
+	PostToPIXNET ptp = new PostToPIXNET("46857444204aa2ce0cb8c9666a8eadae",
+				"dfa46d6dc4acfc8a25046fffcc5d9b14");
+	System.out.println(ptp.set_nonce());
+    }  
+    @Test
+    public void set_timestamp() throws Exception{
+	PostToPIXNET ptp = new PostToPIXNET("46857444204aa2ce0cb8c9666a8eadae",
+				"dfa46d6dc4acfc8a25046fffcc5d9b14");
+	System.out.println(ptp.set_timestamp());
+    } 
+    @Test
+    public void get() throws Exception{
+	PostToPIXNET ptp = new PostToPIXNET("46857444204aa2ce0cb8c9666a8eadae",
+				"dfa46d6dc4acfc8a25046fffcc5d9b14");
+	assertEquals(ptp.getOauthToken(),"");
+    } 
+}
