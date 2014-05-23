886e42525c087a022599250563ee86f67c8d15a9
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-05-23 17:06:37 +0800

    update test

---
+++ Test/src/main/java/Main.java
@@ -0,0 +1,7 @@
+import com.pixnet.*;
+public class Main{
+	public static void main(String[] args) throws Exception{
+		System.out.println("Test");
+		PostToPIXNETOauth2 ptp = new PostToPIXNETOauth2("","","");
+	}
+}
