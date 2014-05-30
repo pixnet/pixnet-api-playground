5cf882c3ac937d3f2387a6d95b95eead71969232
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-05-30 13:18:36 +0800

    Test : Get AccessToken from setUp

--- PIXNET_Oauth1.0a/src/test/java/com/example/MainTest.java
+++ PIXNET_Oauth1.0a/src/test/java/com/example/MainTest.java
@@ -12,11 +12,13 @@ import static org.hamcrest.CoreMatchers.*;
 public class MainTest {
 	PostToPIXNET ptp;
 	PostToPIXNETOauth2 ptp2;
+	String accessToken;
     @Before public void setUp() throws Exception{
 	ptp = new PostToPIXNET("46857444204aa2ce0cb8c9666a8eadae",
 				"dfa46d6dc4acfc8a25046fffcc5d9b14");
 	ptp2 = new PostToPIXNETOauth2("e6a0fa232cc4da68ae21b727b772229e",
 				"fac5c7f719feef5eea37449b1fc6b2ad","http://oob");
+	accessToken = ptp2.getAccessToken("caa9be1c0017b0296fd4335b629a3c6316e5fd72");
     }
     @Test
     public void PostToPIXNETOauth2TestRequest() throws Exception{  
@@ -24,7 +26,7 @@ public class MainTest {
     } 
     @Test 
     public void PostToPIXNETOauth2TestPostError() throws Exception{
-	String responseString = ptp2.post("047e33c25a828b9daaf22181eb5abb04","Test","AAA","");
+	String responseString = ptp2.post(accessToken,"Test","AAA","");
 	try{
 		assertThat(responseString, both(containsString("\"body\":\"HiHi\"")).and(containsString("\"title\":\"Test\"")));
 	}catch(AssertionError e){
@@ -33,7 +35,7 @@ public class MainTest {
     } 
     @Test 
     public void PostToPIXNETOauth2TestPost() throws Exception{
-	String responseString = ptp2.post("047e33c25a828b9daaf22181eb5abb04","Test","AAA","");
+	String responseString = ptp2.post(accessToken,"Test","AAA","");
 	try{
 		assertThat(responseString, both(containsString("\"body\":\"AAA\"")).and(containsString("\"title\":\"Test\"")));
 	}catch(AssertionError e){
