6cab287ef5b15fc0d4fe9c17fa845a3461bde67c
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-05-30 18:10:52 +0800

    can use api ,  get RequestUrl , post not done yet

--- PIXNET_Android/src/com/pixnet/pixnetapi/MainActivity.java
+++ PIXNET_Android/src/com/pixnet/pixnetapi/MainActivity.java
@@ -13,31 +13,57 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import com.pixnet.*;
 
 public class MainActivity extends Activity {
+	PostToPIXNETOauth2 ptp;
+	TextView show;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//try {
-		//	PostToPIXNETOauth2 ptp = new PostToPIXNETOauth2(
-		//			"e6a0fa232cc4da68ae21b727b772229e",
-		//			"fac5c7f719feef5eea37449b1fc6b2ad", "http://oob");
-		//	TextView show = (TextView) findViewById(R.id.show);
-		//	show.setText(ptp.getRequestUrl());
-		//} catch (IOException e) {
+		try {
+			ptp = new PostToPIXNETOauth2("e6a0fa232cc4da68ae21b727b772229e",
+					"fac5c7f719feef5eea37449b1fc6b2ad", "http://oob");
+			sendMessage();
+		} catch (IOException e) {
 
-		//} catch (JSONException e) {
-//
-		//}
+		} catch (JSONException e) {
+
+		}
 
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
+
+	private void sendMessage() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				show = (TextView) findViewById(R.id.show);
+				System.out.println(ptp.getRequestUrl());
+				Message msg = new Message();
+				Bundle se = new Bundle();
+				se.putString("in", ptp.getRequestUrl());
+				msg.setData(se);
+				mHandler.sendMessage(msg);
+			}
+		}).start();
+	}
+
+	private Handler mHandler = new Handler() {
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			show.setText(msg.getData().getString("in"));
+
+		}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
